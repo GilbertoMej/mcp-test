f982553aba8029c3ee2eced9854b441c242e72c3
GilbertoMej <gilberto.mejia@colimasoft.com>
2026-08-09 19:29:48 -0600

    [Feature] Step 3: Server connection managment

--- weather-client/index.ts
+++ weather-client/index.ts
@@ -20,17 +20,57 @@ if (!OPENROUTER_API_KEY) {
 class MCPClient {
   private mcp: Client;
   private openrouter: OpenAI;
-  private transport: StdioClientTransport;
+  private transport: StdioClientTransport | null = null;
   private tools: ChatCompletionTool[] = [];
 
   constructor() {
     this.openrouter = new OpenAI({
-        baseURL: "https://openrouter.ai/api/v1",
-        apiKey: OPENROUTER_API_KEY,
+      baseURL: "https://openrouter.ai/api/v1",
+      apiKey: OPENROUTER_API_KEY,
     });
 
     this.mcp = new Client({ name: "mcp-client-cli", version: "1.0.0" });
   }
 
   // Methods will go here
+
+  async connectToServer(serverScriptPath: string) {
+    try {
+      const isJs = serverScriptPath.endsWith(".js");
+      const isPy = serverScriptPath.endsWith(".py");
+      if (!isJs && !isPy) {
+        throw new Error("Server script must be a .js or .py file");
+      }
+      const command = isPy
+        ? process.platform === "win32"
+          ? "python"
+          : "python3"
+        : process.execPath;
+
+      this.transport = new StdioClientTransport({
+        command,
+        args: [serverScriptPath],
+      });
+      await this.mcp.connect(this.transport);
+
+      const toolsResult = await this.mcp.listTools();
+      this.tools = toolsResult.tools.map((tool) => {
+        return {
+          type: "function",
+          function: {
+            name: tool.name,
+            description: tool.description ?? "",
+            parameters: tool.inputSchema,
+          },
+        };
+      });
+      console.log(
+        "Conectado el servidor con las herramientas: ",
+       toolsResult.tools.map((tool) => tool.name)
+      );
+    } catch (e) {
+      console.log("La conexion con el servidor MCP ha fallado: ", e);
+      throw e;
+    }
+  }
 }
